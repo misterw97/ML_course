bec146eb4131486c984db1a0363c052d7bec220c
Valentin Viennot <valentin.viennot@epfl.ch>
2019-12-01 18:55:15 +0100

    lab08

---
+++ labs/ex08/implementation/helper.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""Some helper functions."""
+import os
+import shutil
+import numpy as np
+from scipy import misc
+import imageio
+
+def load_data():
+    """Load data and convert it to the metrics system."""
+    path_dataset = "faithful.csv"
+    data = np.loadtxt(path_dataset, delimiter=" ", skiprows=0)
+    return data
+
+
+def normalize_data(data):
+    """normalize the data by (x - mean(x)) / std(x)."""
+    mean_data = np.mean(data, axis=0)
+    data = data - mean_data
+    std_data = np.std(data)
+    data = data / std_data
+    return data
+
+
+def build_dir(dir):
+    """build a new dir. if it exists, remove it and build a new one."""
+    if os.path.exists(dir):
+        shutil.rmtree(dir)
+    os.makedirs(dir)
+
+
+def load_image(path):
+    """use the scipy.misc to load the image."""
+    return imageio.imread(path)
+
+
+def build_distance_matrix(data, mu):
+    """build a distance matrix.
+    return
+        distance matrix:
+            row of the matrix represents the data point,
+            column of the matrix represents the k-th cluster.
+    """
+    distances = np.empty((data.shape[0], mu.shape[0]))
+    for i, point in enumerate(data):
+        for j, k in enumerate(mu):
+            distances[(i,j)] = np.linalg.norm(point-k)
+    return distances
